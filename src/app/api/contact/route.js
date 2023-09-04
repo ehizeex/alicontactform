19702d43057a80f3631b448c67d8dcbab83afa53
sarabdev <sarab@contrivers.dev>
2023-09-04 16:37:17 +0500

    initial

---
+++ src/app/api/contact/route.js
@@ -0,0 +1,94 @@
+import { NextResponse } from 'next/server'
+ 
+export async function POST(request) {
+    require('dotenv').config()
+     const data=await request.json()
+    let nodemailer = require('nodemailer')
+    try{
+    const transporter = nodemailer.createTransport({
+      port: 465,
+      host: "smtp.gmail.com",
+      service:"gmail",
+      
+      auth: {
+        user: '', //type your email
+        pass: '', //enable 2 factor authentication on gmail and generate a password from App passwords section 
+      },
+     
+    })
+    
+    const info = await transporter.sendMail({
+      from: `"${data.name}"`, 
+      to: "",//type the email where you want to send 
+      subject: `Inquiry from Website Contact Form`, 
+      text: "", 
+      html: `
+      <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .email-container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+      
+        p {
+            font-size: 16px;
+            line-height: 1.5;
+            margin-bottom: 10px;
+        }
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+        li {
+            margin-bottom: 5px;
+        }
+        .signature {
+            font-style: italic;
+            font-size: 14px;
+        }
+      </style>
+</head>
+<body>
+    <div class="email-container">
+
+        <p>Dear,</p>
+
+        <p>I hope this message finds you well. I wanted to share some information with you regarding an inquiry we received through our website contact form. Below are the details provided by the visitor:</p>
+
+        <ul>
+            <li><strong>Name:</strong> ${data.name}</li>
+            <li><strong>Email:</strong> ${data.email}</li>
+            <li><strong>Phone:</strong> ${data.phoneNumber}</li>
+            <li><strong>Subject:</strong> ${data.subject}</li>
+            <li><strong>Message:</strong><br>${data.message}</li>
+        </ul>
+
+
+
+        <p class="signature">Best regards</p>
+    </div>
+</body>
+</html>
+
+      `, 
+    });
+    if(info.messageId){
+      return NextResponse.json({ error:false, message:"Message successfully sent!" })
+
+    }
+    return NextResponse.json({ error:true, message:'Something went wrong please try again.' })
+
+  }
+  catch(e){
+    return NextResponse.json({ error:true, message:'Something went wrong please try again.' })
+
+  }
+  }
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,271 @@
+"use client"
 import Image from 'next/image'
-
+import { useEffect, useState } from 'react';
+import { FaLocationDot } from "react-icons/fa6";
+import { FiPhoneCall } from "react-icons/fi";
+import { AiFillMail, AiOutlineFieldTime } from "react-icons/ai"
+import { FaHeadphones } from "react-icons/fa";
 export default function Home() {
+  const [state,setState]=useState({
+    name:'',
+    email:'',
+    phoneNumber:'',
+    subject:'',
+    message:'',
+  })
+  const [successMessage,setSuccessMessage]=useState("")
+  const [errorMessage,setErrorMessage]=useState("")
+  const [loading,setLoading]=useState(false)
+
+  
+  const handleChange=(e)=>{
+    const key=e.target.name;
+    const value=e.target.value;
+
+    setState({
+      ...state,
+      [key]:value
+    })
+  }
+
+  const clearState=()=>{
+    setState({
+      name:'',
+      email:'',
+      phoneNumber:'',
+      subject:'',
+      message:'',
+     
+    })
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    setLoading(true)
+    let data = {
+      ...state
+    }
+    fetch('/api/contact', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then(async(res) => {
+      setLoading(false)
+      const response= await res.json()
+       if(!response.error){
+         
+         clearState()
+         setSuccessMessage(response.message)
+         setErrorMessage('')
+       }
+      else{
+        clearState()
+        setErrorMessage(response.message)
+        setSuccessMessage('')
+      }
+    }).catch(e=>{
+       clearState()
+       setLoading(false)
+       setErrorMessage("Something went wrong try again.")
+       setSuccessMessage('')
+    })
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+    <div className="bg-gray-100 flex flex-col items-center justify-center pt-[80px] pb-[80px] px-[20px]">
+    <div className="text-center bg-red-600 py-[1px] px-[8px] tracking-wide uppercase font-semibold text-[14px] text-white">
+      Contact Details
+    </div>
+    <div className="mt-[10px]">
+      <p className="md:text-[35px] sm:text-[30px] text-[25px] text-[#204669] font-semibold tracking-wide text-center">
+        We Would Love To Help You !
+      </p>
+    </div>
+    <div className="mt-[10px] md:w-[700px] w-auto text-center">
+      <p>
+        Turpis wisi pede tempus assumenda pede quis pretium. Aspernatur
+        dolorem quod ultricies dicta ipsa, culpa condimentum do exercitation
+        molestie, tincidunt quisque in eum, maecenas.
+      </p>
+    </div>
+    <div className="sm:flex gap-[30px] mt-[30px]">
+      <div className="flex xl:flex-row flex-col gap-[15px]">
+        <div className="flex flex-col gap-[15px]  py-[35px] px-[45px]  bg-white shadow-lg">
+          <div className="flex items-center gap-[15px]">
+            <div>
+              <FaLocationDot size={40} color="red" />
+            </div>
+            <div className="flex flex-col">
+              <div className="text-red-500 text-[14px]">LOCATION...</div>
+              <div className="font-semibold text-[20px] tracking-wide ">
+                Visit Us At
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="text-[#666666]">65th Street, Los Angeles</div>
+            <div className="text-[#666666]"> 3rd Street, San Andreas,</div>
+          </div>
+        </div>
+        <div className="flex flex-col  py-[35px] px-[45px] gap-[15px] bg-white shadow-lg">
+          <div className="flex items-center gap-[15px]">
+            <div>
+              <FiPhoneCall size={40} color="red" />
+            </div>
+            <div className="flex flex-col">
+              <div className="text-red-500 text-[14px]">24*7 SERVICE..</div>
+              <div className="font-semibold text-[20px] tracking-wide ">
+                Call Us On
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="text-[#666666] ">Tel: +81 028874-83</div>
+            <div className="text-[#666666]">Mob: +83 84793-43</div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex  xl:flex-row flex-col gap-[15px]">
+        <div className="flex flex-col gap-[15px] py-[35px] px-[45px]  bg-white shadow-lg sm:mt-0 mt-[15px]">
+          <div className="flex items-center gap-[15px]">
+            <div>
+              <AiFillMail size={40} color="red" />
+            </div>
+            <div className="flex flex-col">
+              <div className="text-red-500 text-[14px]">Drop A Line</div>
+              <div className="font-semibold text-[20px] tracking-wide ">
+                Mail Address
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="text-[#666666]">info@Domain.com</div>
+            <div className="text-[#666666]">Domain@Company.com</div>
+          </div>
+        </div>
+        <div className="flex flex-col py-[35px]  px-[45px] gap-[15px] bg-white shadow-lg">
+          <div className="flex items-center gap-[15px]">
+            <div>
+              <AiOutlineFieldTime size={40} color="red" />
+            </div>
+            <div className="flex flex-col">
+              <div className="text-red-500 text-[14px]">OFFICE HOURS..</div>
+              <div className="font-semibold text-[20px] tracking-wide ">
+                Opening Time
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="text-[#666666]">Mon – Fri : 9am – 6pm</div>
+            <div className="text-[#666666]"> Sunday (Closed)</div>
+          </div>
+        </div>
       </div>
+    </div>
+  </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+  <div className="flex flex-col xl:flex-row justify-center gap-[60px] bg-white mt-[60px] px-[30px]">
+      <div
+        className="relative w-full xl:w-[35 0px] h-[450px] bg-center bg-cover"
+        style={{ backgroundImage: `url('/backgroundImage.jpg')` }}>
+        <div className="absolute h-full w-full bg-gradient-to-t from-blue-900 via-blue-900 shadow-inner opacity-80"></div>
+        <div className="absolute  inset-0 flex flex-col gap-[20px] items-center justify-center text-white shadow-lg p-6">
+          <div className="text-4xl xl:mt-[150px] mt-0  rounded-full px-[15px] py-[15px] bg-blue-900 text-white">
+            <FaHeadphones />
+          </div>
+          <div className=" font-semibold text-[24px]">Chat With Live!</div>
+          <div>
+            <p className="text-center">
+              Porro. Erat gravida adipisci quibusdam faucibus diam molestias?
+              Ante, arcu commodo, non! Phasellus risus tenetur.
+            </p>
+          </div>
+          <div>
+            <button className="bg-red-500 text-white mb-[30px]  px-4 py-2  hover:bg-red-700 transition-colors">
+              LET'S CHAT
+            </button>
+          </div>
+        </div>
       </div>
-    </main>
+      <div className="flex flex-col gap-[20px]">
+        <div className="text-center w-[100px] bg-red-600 py-[1px]  tracking-wide uppercase font-semibold text-[14px] text-white">
+          Contact Us
+        </div>
+        <div>
+          <p className="text-[30px]">
+            Request A Call Back ! Feel Free To Reach & Contact Us.
+          </p>
+        </div>
+        <form className="flex flex-col gap-[20px]" onSubmit={handleSubmit} >
+          <div className="flex flex-col sm:flex-row gap-[20px]">
+            <input
+              type="text"
+              name="name"
+              onChange={handleChange}
+              placeholder="Your Name.."
+              value={state.name}
+              required
+              className="px-[12px] outline-none  rounded-md py-[12px] flex-1 bg-gray-200"
+            />
+            <input
+            name="email"
+              type="email"
+              required
+              onChange={handleChange}
+              value={state.email}
+              placeholder="Your Email.."
+              className="px-[12px] outline-none  rounded-md py-[12px] flex-1 bg-gray-200"
+            />
+          </div>
+          <div className="flex flex-col sm:flex-row  gap-[20px]">
+            <input
+            name="phoneNumber"
+              type="text"
+              value={state.phoneNumber}
+              required
+              onChange={handleChange}
+
+              placeholder="Your Number.."
+              className="px-[12px] outline-none  rounded-md py-[12px] flex-1 bg-gray-200"
+            />
+            <input
+              type="text"
+              name="subject"
+              placeholder="Your Subject.."
+              required
+              onChange={handleChange}
+
+              value={state.subject}
+              className="px-[12px] outline-none  rounded-md py-[12px] flex-1 bg-gray-200"
+            />
+          </div>
+          <div>
+            <textarea
+            required
+            name="message"
+            onChange={handleChange}
+
+            value={state.message}
+              placeholder="Your Message"
+              className="px-[12px] outline-none h-[80px] w-full  rounded-md py-[12px]  bg-gray-200"
+            />
+          </div>
+          <div>
+            {successMessage && <p className="text-green-600 font-bold p-2" >{successMessage}</p>}
+            {errorMessage && <p className="text-red-600 font-bold p-2" >{errorMessage}</p>}
+            {loading && <div className=" mb-3 ml-3 w-6 h-6 border-t-2 border-blue-500 border-solid rounded-full animate-spin"></div>}
+
+            <button  className="bg-blue-900 sm:w-auto w-full px-[35px] py-[12px] hover:bg-red-800 transition-colors font-semibold text-white duration-500">
+              SEND YOU MESSAGE
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </>
   )
 }
